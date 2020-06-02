9fc928f1c41f9d751d0f3b3d175b54eeba4d4587
dickyrey <dicky.rey97@gmail.com>
2020-06-03 03:57:48 +0700

    barbershop app

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:barber/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Barbershop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
